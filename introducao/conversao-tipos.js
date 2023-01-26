cf867ed6b2ecd757a0ad7860eb589e5622ddc7ba
Matheus Malta de Aguiar <maltamatheus@gmail.com>
2023-01-26 20:42:36 -0300

    Commit até a aula 26

---
+++ introducao/conversao-tipos.js
@@ -0,0 +1,3 @@
+int = 8
+valorHexa = int.toString(8)
+console.log(valorHexa, typeof valorHexa)
--- introducao/primeiro-exemplo.js
+++ introducao/primeiro-exemplo.js
@@ -1,1 +1,3 @@
-console.log("Primeiro Exemplo Javascript!");
+// console.log("Primeiro Exemplo Javascript!");
+const variavel = true
+console.log(`O valor de variavel é ${variavel} e seu tipo é ${typeof variavel}` )
